578cb5387a89e7d1338cdca73c95b74c9a3cb838
Mohammed-bintaleb <engineerbintaleb@gmail.com>
2025-07-07 15:13:03 +0300

    Onyx Dashboard

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const OnyxDashbord());
+}
+
+class OnyxDashbord extends StatelessWidget {
+  const OnyxDashbord({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
