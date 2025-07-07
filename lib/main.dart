2b9543c58aad6f179218b136efc9915e797a79a5
Mohammed-bintaleb <engineerbintaleb@gmail.com>
2025-07-07 15:23:02 +0300

    Create dashboard tablet layout file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:onyx_dashboard/views/widgets/dash_board_view.dart';
+import 'package:onyx_dashboard/views/dash_board_view.dart';
 
 void main() {
   runApp(const OnyxDashbord());
--- lib/views/dash_board_view.dart
+++ lib/views/dash_board_view.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:onyx_dashboard/views/widgets/adaptive_layout_widget.dart';
+import 'package:onyx_dashboard/views/widgets/dashboard_tablet_layout.dart';
 
 class DashBoardView extends StatelessWidget {
   const DashBoardView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: AdaptiveLayout(
+        mobileLayout: (context) => SizedBox(),
+        tabletLayout: (context) => DashboardTabletLayout(),
+      ),
+    );
   }
 }
