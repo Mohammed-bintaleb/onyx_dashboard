fdd9444c4ffbaa39591877b76d6a271d0691b849
Mohammed-bintaleb <engineerbintaleb@gmail.com>
2025-07-12 16:37:27 +0300

    Add localization support to CustomContainerListView text

--- lib/features/home/presentation/views/widgets/adaptive_layout_widget.dart
+++ lib/features/home/presentation/views/widgets/adaptive_layout_widget.dart
@@ -1,24 +1,24 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-class AdaptiveLayout extends StatelessWidget {
-  const AdaptiveLayout({
-    super.key,
-    required this.mobileLayout,
-    required this.tabletLayout,
-  });
+// class AdaptiveLayout extends StatelessWidget {
+//   const AdaptiveLayout({
+//     super.key,
+//     required this.mobileLayout,
+//     required this.tabletLayout,
+//   });
 
-  final WidgetBuilder mobileLayout, tabletLayout;
+//   final WidgetBuilder mobileLayout, tabletLayout;
 
-  @override
-  Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (context, constrains) {
-        if (constrains.maxWidth < 800) {
-          return mobileLayout(context);
-        } else {
-          return tabletLayout(context);
-        }
-      },
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return LayoutBuilder(
+//       builder: (context, constrains) {
+//         if (constrains.maxWidth < 800) {
+//           return mobileLayout(context);
+//         } else {
+//           return tabletLayout(context);
+//         }
+//       },
+//     );
+//   }
+// }
--- lib/features/home/presentation/views/widgets/custom_container_list_view.dart
+++ lib/features/home/presentation/views/widgets/custom_container_list_view.dart
@@ -1,41 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:onyx_dashboard/core/utils/app_localizations.dart';
 import '../../../../customer/data/models/custom_container_model.dart';
 import 'custom_container.dart';
 
 class CustomContainerListView extends StatelessWidget {
   const CustomContainerListView({super.key});
 
-  static const items = [
-    CustomContainerModel(
-      title: 'Total Orders Today',
-      value: '125',
-      trendText: '+12% from last period',
-      titleIcon: Icons.shopping_cart_outlined,
-    ),
-    CustomContainerModel(
-      title: 'Active Customers',
-      value: '1,200',
-      trendText: '+50 from last period',
-      titleIcon: Icons.people_alt_outlined,
-    ),
-    CustomContainerModel(
-      title: 'Revenue This Week',
-      value: '\$15,231.89',
-      trendText: '+8.2% from last period',
-      titleIcon: Icons.attach_money_rounded,
-    ),
-    CustomContainerModel(
-      title: 'Top Selling Product',
-      value: 'Pro Widget',
-      trendText: 'Model #X-23 from last period',
-      titleIcon: Icons.inventory,
-    ),
-  ];
-
   @override
   Widget build(BuildContext context) {
+    final t = AppLocalizations.of(context)!;
     final screenWidth = MediaQuery.of(context).size.width;
     final isLargeScreen = screenWidth >= 800;
+
+    final items = [
+      CustomContainerModel(
+        title: t.translate('total_orders_today'),
+        value: '125',
+        trendText: t.translate('trend_orders_today'),
+        titleIcon: Icons.shopping_cart_outlined,
+      ),
+      CustomContainerModel(
+        title: t.translate('active_customers'),
+        value: '1,200',
+        trendText: t.translate('trend_active_customers'),
+        titleIcon: Icons.people_alt_outlined,
+      ),
+      CustomContainerModel(
+        title: t.translate('revenue_this_week'),
+        value: '\$15,231.89',
+        trendText: t.translate('trend_revenue'),
+        titleIcon: Icons.attach_money_rounded,
+      ),
+      CustomContainerModel(
+        title: t.translate('top_selling_product'),
+        value: 'Pro Widget',
+        trendText: t.translate('trend_top_product'),
+        titleIcon: Icons.inventory,
+      ),
+    ];
 
     if (isLargeScreen) {
       return SingleChildScrollView(
