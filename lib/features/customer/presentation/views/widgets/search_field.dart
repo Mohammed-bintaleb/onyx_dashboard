0298ec073f89b99348cdc97a3696b3c15093a691
Mohammed-bintaleb <engineerbintaleb@gmail.com>
2025-07-12 17:45:14 +0300

    Add localization support to OrderTableSection text part2

--- lib/features/customer/presentation/views/widgets/search_field.dart
+++ lib/features/customer/presentation/views/widgets/search_field.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../../../../../core/utils/app_localizations.dart';
 
 class SearchField extends StatelessWidget {
   final TextEditingController? controller;
@@ -8,11 +9,13 @@ class SearchField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final t = AppLocalizations.of(context)!;
+
     return TextField(
       controller: controller,
       onChanged: onChanged,
       decoration: InputDecoration(
-        hintText: 'Search by name or order ID...',
+        hintText: t.translate('search_hint'),
         prefixIcon: const Icon(Icons.search),
         border: OutlineInputBorder(borderRadius: BorderRadius.circular(8)),
         contentPadding: const EdgeInsets.symmetric(horizontal: 16),
